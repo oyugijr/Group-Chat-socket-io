056b55f0734ff97c3cd97307f4eac96ffa61d7b2
okwareddevnest <softengdedan@gmail.com>
2025-06-27 17:26:02 +0300

    baseurl for backend changed

--- client/src/services/backenInt.js
+++ client/src/services/backenInt.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 
-const BackendBaseUrl = "http://localhost:5000";
-const APIBaseUrl = "http://localhost:5000/api";
+const BackendBaseUrl = "https://group-chat-socket-io-p13f.onrender.com";
+const APIBaseUrl = "https://group-chat-socket-io-p13f.onrender.com/api";
 
 const API = axios.create({
     baseURL: APIBaseUrl,
