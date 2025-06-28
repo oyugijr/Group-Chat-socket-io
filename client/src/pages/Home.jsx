cce5e96b2c8b1f1217a3efa61bdbd3c43822e12e
oyugi <oyugimaurice22@gmail.com>
2025-06-28 14:31:50 +0300

    refactor: update package.json start script and clean imports in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getRooms, createRoom, getMessages, socket } from "../services/backenInt";
+import { getRooms, getMessages, socket } from "../services/backenInt";
 import ChatRoom from "../components/ChatRoom";
 
 
